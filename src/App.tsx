c619639cd17c0c1f989368d40764ad77039dfbd6
Bozhidar Andreev <33715265+bojidarandreev@users.noreply.github.com>
2025-09-02 22:45:25 +0200

    refactor(auth): integrate main layout with protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import LoginPage from './pages/Login';
 import DashboardPage from './pages/Dashboard';
 import { AuthProvider } from './components/AuthProvider';
 import ProtectedRoute from './components/ProtectedRoute';
-import useAuthStore from './store/auth';
+import Layout from './components/Layout';
 
 const queryClient = new QueryClient();
 
@@ -12,27 +12,26 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <AppRoutes />
+        <Router>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route 
+              element={
+                <ProtectedRoute>
+                  <Layout>
+                    <Outlet />
+                  </Layout>
+                </ProtectedRoute>
+              }
+            >
+              <Route path="/" element={<DashboardPage />} />
+              {/* Add other protected routes here */}
+            </Route>
+          </Routes>
+        </Router>
       </AuthProvider>
     </QueryClientProvider>
   );
 }
-
-// A separate component to access the auth state
-const AppRoutes = () => {
-  const { isAuthenticated } = useAuthStore();
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<DashboardPage />} />
-          {/* Add other protected routes here */}
-        </Route>
-      </Routes>
-    </Router>
-  );
-};
 
 export default App;
