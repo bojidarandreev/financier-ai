13275ee278c9b7d60213fb5beea3c9434654faad
Bozhidar Andreev <33715265+bojidarandreev@users.noreply.github.com>
2025-09-02 22:48:50 +0200

    feat(routing): add transactions page route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import LoginPage from './pages/Login';
 import DashboardPage from './pages/Dashboard';
+import TransactionsPage from './pages/Transactions';
 import { AuthProvider } from './components/AuthProvider';
 import ProtectedRoute from './components/ProtectedRoute';
 import Layout from './components/Layout';
@@ -25,6 +26,7 @@ function App() {
               }
             >
               <Route path="/" element={<DashboardPage />} />
+              <Route path="/transactions" element={<TransactionsPage />} />
               {/* Add other protected routes here */}
             </Route>
           </Routes>
