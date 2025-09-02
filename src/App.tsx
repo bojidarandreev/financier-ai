bbb4c2f08c5945b69add482b3b25bb02073fe400
Bozhidar Andreev <33715265+bojidarandreev@users.noreply.github.com>
2025-09-02 23:03:33 +0200

    refactor(debug): simplify routing to debug build issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import LoginPage from './pages/Login';
 import DashboardPage from './pages/Dashboard';
 import TransactionsPage from './pages/Transactions';
 import AccountsPage from './pages/Accounts';
 import CategoriesPage from './pages/Categories';
-import { AuthProvider } from './components/AuthProvider';
-import ProtectedRoute from './components/ProtectedRoute';
 import Layout from './components/Layout';
 
 const queryClient = new QueryClient();
@@ -14,27 +12,15 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <Router>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route 
-              element={
-                <ProtectedRoute>
-                  <Layout>
-                    <Outlet />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            >
-              <Route path="/" element={<DashboardPage />} />
-              <Route path="/transactions" element={<TransactionsPage />} />
-              <Route path="/accounts" element={<AccountsPage />} />
-              <Route path="/categories" element={<CategoriesPage />} />
-            </Route>
-          </Routes>
-        </Router>
-      </AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<Layout><DashboardPage /></Layout>} />
+          <Route path="/transactions" element={<Layout><TransactionsPage /></Layout>} />
+          <Route path="/accounts" element={<Layout><AccountsPage /></Layout>} />
+          <Route path="/categories" element={<Layout><CategoriesPage /></Layout>} />
+        </Routes>
+      </Router>
     </QueryClientProvider>
   );
 }
